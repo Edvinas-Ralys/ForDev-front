548b70168b884b9823ff9a3f5ff4faaa597e019f
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2024-04-11 17:08:01 +0300

    Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { PostProvider } from "./Contexts/Post"
 import { RouterProvider } from "./Contexts/Router"
 import { PrimeReactProvider, PrimeReactContext } from "primereact/api"
 import "./styles/main.scss"
-import { LoadingContextProvider } from "./Contexts/LoadingCont"
 import { CommentProvider } from "./Contexts/Comment"
 import { ProfileProvider } from "./Contexts/Profile"
 
